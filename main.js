226f6d578bc89b8a8b9050c6a7e3766a99038a63
“Allie <allieroll22@ufl.edu>
2019-02-22 12:11:53 -0500

    Print weather description

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 var elementSpace = document.getElementById('place');
-console.log(elementSpace);
+
+var weatherSpace = document.getElementById('weather');
 
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var apiResult = JSON.parse(this.responseText);
 
-        //Code Dealing With The API Data Goes Here
-        console.log(apiResult.name);
         var cityName = document.createTextNode(apiResult.name);
         elementSpace.appendChild(cityName);
 
+        var weatherToday = document.createTextNode(apiResult.weather[0].description);
+        weatherSpace.appendChild(weatherToday);
     }
 };
 xmlhttp.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=32801,us&appid=6efff70fe1477748e31c17d1c504635f', true);
